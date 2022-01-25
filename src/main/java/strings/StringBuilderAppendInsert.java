1fa3ff081257a715b6fed2d473da03645bbd88a8
joshuabaroni <joshua.baroni.16@cnu.edu>
2022-01-25 16:30:31 -0500

    sb

---
+++ src/main/java/strings/StringBuilderAppendInsert.java
@@ -0,0 +1,8 @@
+package strings;
+
+public class StringBuilderAppendInsert {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder(100);
+        sb.append("hello");
+    }
+}
